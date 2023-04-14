caaf559a4aa83582ac97475a4b3d14a3cadbd72d
A-Maksimchuk <a_maksimchuk@outlook.com>
2023-04-14 13:41:16 +0300

    Preparation for work
    Remove Zenject extras, import skyboxes asset(BOXOPHOBIC), create main folder architecture

---
+++ HitTheBall/Assets/_CodeBase/HitTheBall/ZenjectInstallers/ProjectInstaller.cs
@@ -0,0 +1,11 @@
+using Zenject;
+
+namespace HitTheBall.ZenjectInstallers
+{
+    public class ProjectInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+        }
+    }
+}
